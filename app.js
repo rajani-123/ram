9e2e0aae2030cc5327b724f3c53b7ed302357863
Akhilanand Singh <akhilanandsingh@gmail.com>
2016-03-06 21:57:23 +0530

    my commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var exp = require("express");
+var app = exp();
+
+app.get('/players', function(req, res){
+	var query = req.query;
+	console.log("query value" + JSON.stringify(query));
+	res.write("name=" +query.name);
+	res.end("   lang=" +query.lang);
+	res.end(JSON.stringify(query));
+});
+var port = process.env.PORT || 4000;
+app.listen(port, function(){
+	console.log("server is listening on port " + port);
+});
---
+++ app3.js
@@ -0,0 +1,15 @@
+var exp = require("express");
+var app = exp();
+
+app.use(exp.static(__dirname + "/public"));
+
+
+app.get('/players/:lang/:name', function(req, res){
+	
+	res.write("name=" + req.params.name);
+	res.end("   lang=" +req.params.lang);
+});
+var port = process.env.PORT || 4000;
+app.listen(port, function(){
+	console.log("server is listening on port " + port);
+});
